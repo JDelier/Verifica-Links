7c01e900ed386a1fbad5bab3a4883258268dd70e
Jdelier <jdelierr@gmail.com>
2023-08-31 03:56:14 -0300

    some minor fixes

--- src/http-validacao.js
+++ src/http-validacao.js
@@ -1,35 +1,28 @@
-function extraiLink(arrLinks)
-{
-    const arrResult = [];
-    arrLinks.map((objLink) =>
-    {
-        arrResult.push(Object.values(objLink).join(''))
+function extraiLink(arrLinks) {
+  const arrResult = [];
+  arrLinks.map((objLink) => {
+    arrResult.push(Object.values(objLink).join(""));
+  });
+
+  return arrResult;
+}
+
+async function checkForStatus(arrUrls) {
+  const arrStatus = await Promise.all(
+    arrUrls.map(async (url) => {
+      const response = await fetch(url ,{ method: 'HEAD'});
+      return response.status;
     })
+  );
 
-    return arrResult;
-
+  return await arrStatus;
 }
 
-async function checkForStatus(arrUrls)
-{   
-    const arrStatus = await Promise.all(
-        arrUrls.map(async (url) => {
-        const response = await fetch(url);    
-        return response.status;
-        })
-    )
-    
-    return await arrStatus
+export default async function verifiedList(list) {
+  const links = extraiLink(list);
+  const status = await checkForStatus(links);
+  return status;
 }
-
-
-export default async function verifiedList(list)
-{
-    const links =   extraiLink(list)
-    const status = await checkForStatus(links)
-    return status
-}
-
 
 // const res = await fetch('https://nodejs.org/api/documentation.json');
 // if (res.ok) {
--- src/index.js
+++ src/index.js
@@ -1,49 +1,34 @@
-import fs, { fstatSync, readFile } from 'fs'
-import chalk from 'chalk';
+import fs, { fstatSync, readFile } from "fs";
+import chalk from "chalk";
 
 const log = console.log;
 
-
-
-async function takeFile(path)
-{   
-    
-    try {
-        const enconding = 'utf-8' 
-        const texto = await fs.promises.readFile(path, enconding)
-        return extraiLink(texto)
-    }
-    catch(erro)
-    {
-        trataError(erro)
-    }
-    finally 
-    {
-        console.log(chalk.yellow('operação concluída'));
-    }
+async function takeFile(path) {
+  try {
+    const enconding = "utf-8";
+    const texto = await fs.promises.readFile(path, enconding);
+    return extraiLink(texto);
+  } catch (erro) {
+    trataError(erro);
+  } finally {
+    log(chalk.yellow("operação concluída"));
+  }
 }
 
-function trataError(erro)
-{   
-    
-    throw new Error(chalk.red(erro.errno, 'no such file found on this dir'))
-    
+function trataError(erro) {
+  throw new Error(chalk.red(erro.errno, "no such file found on this dir"));
 }
 
-function extraiLink(texto)
-{
-    const regex = /\[([^\[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm; 
+function extraiLink(texto) {
+  const regex = /\[([^\[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
 
-    const capturas =[...texto.matchAll(regex)] 
-    
-    const result = capturas.map(captura => ({[captura[1]]: captura[2]}))
-    
-    return result.length !== 0? result : "Sem match de links no arquivo passado"
+  const capturas = [...texto.matchAll(regex)];
+
+  const result = capturas.map((captura) => ({ [captura[1]]: captura[2] }));
+
+  return result.length !== 0 ? result : "Sem match de links no arquivo passado";
 }
 
 export default takeFile;
-
-
-
 
 // [gatinho salsicha](http://gatinhosalsicha.com.br/)
